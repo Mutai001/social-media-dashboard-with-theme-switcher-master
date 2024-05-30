0ebb72e4e69ec9d058a8b28928e775ffbbd2a1e9
Mutai001 <omusekim@gmail.com>
2024-05-30 15:02:56 +0300

    my commit

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-function darkMode() {
-    let checkbox = document.getElementById('check');
-    let darkmode = document.getElementById('darkmode');
-    if (checkbox.checked) {
-        darkmode.style.display = "flex";
-    } else {
-        darkmode.style.display = "none";
-    }
-}
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleSwitch = document.getElementById('switch');
+    const body = document.body;
+    const headSection = document.querySelector('.head-setion');
+    const topContainers = document.querySelectorAll('.top-container,.top-medias, .insta-container, .youtube');
+    const wrappers = document.querySelectorAll('.wrapper');
+    const themeText = document.getElementById('theme-text');
+
+    toggleSwitch.addEventListener('change', function() {
+        if (toggleSwitch.checked) {
+            body.classList.add('light-theme');
+            headSection.classList.add('light-theme');
+            topContainers.forEach(container => container.classList.add('light-theme'));
+            wrappers.forEach(wrapper => wrapper.classList.add('light-theme'));
+            themeText.textContent = 'Night Mode';
+        } else {
+            body.classList.remove('light-theme');
+            headSection.classList.remove('light-theme');
+            topContainers.forEach(container => container.classList.remove('light-theme'));
+            wrappers.forEach(wrapper => wrapper.classList.remove('light-theme'));
+            themeText.textContent = 'Light Mode';
+        }
+    });
+});
